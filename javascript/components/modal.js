c3040673e9884aba98b980943264bed61e80af8c
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2022-03-30 14:10:50 +0200

    nav underline

--- javascript/components/modal.js
+++ javascript/components/modal.js
@@ -1,15 +1,15 @@
 // referanser til modalen/variabler for elements
-var modal = document.getElementById("modalId");
+const modal = document.getElementById("modalId");
 // til alle bildene, må bruke class istedet for id.
-var bilder = document.getElementsByClassName("images");
+const bilder = document.getElementsByClassName("images");
 // bildet som er i modal
-var modalBilde = document.getElementById("img");
+const modalBilde = document.getElementById("img");
 // caption teksten som skal vises i modal
-var altText = document.getElementById("caption");
+const altText = document.getElementById("caption");
 
 // loop`e igjennom alle bildene som har images classen
-for (var i = 0; i < bilder.length; i++) {
-  var img = bilder[i];
+for (let i = 0; i < bilder.length; i++) {
+  let img = bilder[i];
   // legg til onclick event på hver enkelt bilde.
   img.onclick = function () {
     modal.style.display = "block";
@@ -19,7 +19,7 @@ for (var i = 0; i < bilder.length; i++) {
   };
 }
 
-var spanIcon = document.getElementsByClassName("closemodal")[0];
+const spanIcon = document.getElementsByClassName("closemodal")[0];
 
 spanIcon.onclick = function () {
   modal.style.display = "none";
